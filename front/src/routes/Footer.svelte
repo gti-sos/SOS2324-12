427334777bc9474e33891bbe7167cc0403489b38
josmarsua <josemartinezsuarez0@gmail.com>
2024-03-20 15:10:09 +0100

    Adding Header&Footer

---
+++ front/src/routes/Footer.svelte
@@ -0,0 +1,6 @@
+<style>
+    @import "/styles.css";
+</style>
+<footer style="background-color: white; padding: 20px; text-align: center;">
+    <h1><font size=3>Aplicación realizada por el grupo SOS2324-12 </font></h1>
+</footer>
---
+++ front/src/routes/Header.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { dev } from '$app/environment';
+    import {
+    Nav,
+    NavItem,
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    DropdownMenu,
+    NavLink
+  } from 'sveltestrap';
+
+  let API = '/api/v1';
+    if (dev)
+        API = 'http://localhost:10000'+API;
+
+</script>
+<style>
+    @import "/styles.css";
+</style>
+
+<main> 
+    <Nav tabs class="fixed-navbar" style="background-color: white; justify-content: center;"> 
+        <NavItem class="nav-item"> 
+            <NavLink href="/" class="nav-link d-flex align-items-center"> 
+                <img src="/home-icon.png" alt="Casa" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                Inicio 
+            </NavLink> 
+        </NavItem>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-api.png" alt="API" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                APIS
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "{API}/airbnb-listings">airbnb-listings (Jose Martinez)</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-github.png" alt="GitHub" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                GitHub
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "https://github.com/josmarsua">josmarsua</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-frontend.png" alt="Frontends" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                Frontends
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "/airbnb-listings">airbnb-listings (Jose Martinez)</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-documentacion.png" alt="Documentaciones" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                Documentaciones
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "{API}/airbnb-listings/docs">airbnb-listings (Jose Martinez)</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+    </Nav>
+</main>
