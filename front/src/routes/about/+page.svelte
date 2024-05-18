a12f2a07da6d538048947d20b515c99ed5a85a2e
josmenrue <josmenrue@alum.us.es>
2024-05-18 23:09:04 +0200

    Subida de video Jota

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -38,5 +38,20 @@
 			></iframe>
 			<a href="https://www.youtube.com/watch?v=POnQayIuebA">Ir al vídeo</a>
 		</Container>
+		<hr />
+		<Container style="justify-content: center; text-align: center;">
+			<h1>Jose Joaquin Mena-Bernal Rueda (world-consumption-of-drinking-water-data)</h1>
+			<iframe
+				width="990"
+				height="615"
+				src="https://www.youtube.com/embed/cwzYg1-5jsQ?si=qXEIL_4QomcFxKtC"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+			<a href="https://youtu.be/cwzYg1-5jsQ">Ir al vídeo</a>
+		</Container>
 	</Container>
 </main>
